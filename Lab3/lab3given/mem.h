6bdcfb7ea0e3f249c656ed8c8edb25bcb9215097
Nikman800 <76964182+Nikman800@users.noreply.github.com>
2023-01-06 14:33:41 -0500

    Delete Lab3 directory

--- Lab3/lab3given/mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEM_H
-#define MEM_H
-
-#include <stdlib.h>
-
-void *lab_malloc(size_t nbytes);
-void lab_free(void *);
-
-#endif
--- Lab3/lab3given/util.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-void *checked_malloc(size_t size);
-
-#endif
